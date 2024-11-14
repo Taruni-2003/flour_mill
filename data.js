ef8f7d56183d8ccf5572bae09d210c549df8eaf9
Taruni-2003 <tarunikajayananthan@gmail.com>
2024-11-14 21:14:06 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,41 @@
+import p1_img from'./millets.webp'
+import p2_img from'./all-in-one-spices.jpg'
+import p3_img from'./oil.webp'
+import p4_img from'./wheat powder.webp'
+
+let data_product = [
+    {
+      id:1,
+      name:"Traditional Millet With Healthy Benefit",
+      Image:p1_img,
+      new_price:50.00,
+      old_price:60.00,
+    },
+    {
+        id:2,
+        name :"Buy Whole Homemade Spices From Devi Masala",
+        Image:p2_img,
+        new_price:50.00,
+        old_price:60.00,
+        
+      },
+      {
+        id:3,
+        name :"Nonpolar Chemical",
+        Image:p3_img,
+        new_price:80.00,
+        old_price:99.00,
+        
+      },
+      {
+        id:4,
+        name :"Buy Wheat Flours Online At Best Prices",
+        Image:p4_img,
+        new_price:40.00,
+        old_price:50.00,
+        
+      },
+      
+];
+
+export default data_product;
